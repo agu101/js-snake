0625388a187f80cf514a5abbd5aa1be7ad394b58
agu101 <alan_gu@brown.edu>
2020-10-01 03:40:52 -0400

    snake movement and food

--- snake.js
+++ snake.js
@@ -1,9 +1,14 @@
 /* game constants */
-const SNAKE_SPEED = 2;
-
-/* initial snake and board */
-const snake = [{ x: 16, y: 16 }];
+const SNAKE_SPEED = 5;
 const board = document.getElementById('board');
+
+/* game elements */
+const snake = [{ x: 16, y: 16 }];
+let vx = 0;
+let vy = 0;
+
+let bubble = { x: 0, y: 0 };
+
 
 /* sets up game loop */
 let lastRender = 0;
@@ -24,19 +29,59 @@ function main(time) {
     lastRender = time;
 }
 
+/* takes in user arrow key input */
+window.addEventListener("keydown", e => {
+    switch (e.key) {
+        case 'ArrowUp':
+            if (vy !== 1 || snake.length === 1) { vx = 0; vy = -1; }
+            break
+        case 'ArrowDown':
+            if (vy !== -1 || snake.length === 1) { vx = 0; vy = 1; }
+            break
+        case 'ArrowLeft':
+            if (vx !== 1 || snake.length === 1) { vx = -1; vy = 0; }
+            break
+        case 'ArrowRight':
+            if (vx !== -1 || snake.length === 1) { vx = 1; vy = 0; }
+            break
+    }
+});
+
 /* updates the game logic each loop */
 function update() {
+    /* updates snake position */
+    let head = { x: snake[0].x + vx, y: snake[0].y + vy };
+    snake.unshift(head);
+    /* grows snake if on food */
+    if (snake[0].x === bubble.x && snake[0].y === bubble.y) {
+        console.log("collision");
+        bubble.x = Math.floor(Math.random() * Math.floor(30)) + 1;
+        bubble.y = Math.floor(Math.random() * Math.floor(30)) + 1;
+    } else {
+        snake.pop();
+    }
 
 }
 
 /* renders graphics each loop */
 function render(board) {
+    /* reset board */
+    board.textContent = "";
+    /* draws each snake segment */
     snake.forEach(element => {
         const segment = document.createElement('div');
-        segment.style.gridRowStart = element.x;
-        segment.style.gridColumnStart = element.y;
+        segment.style.gridColumnStart = element.x;
+        segment.style.gridRowStart = element.y;
         segment.classList.add('snake');
         board.appendChild(segment);
     });
+
+    /* draws bubbles */
+    const bub = document.createElement('div');
+    bub.style.gridColumnStart = bubble.x;
+    bub.style.gridRowStart = bubble.y;
+    bub.classList.add('bubble');
+    board.appendChild(bub);
 }
+
 
